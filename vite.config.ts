56f4f2834dd92776759ce4d65ada9d24dd34f81f
Sarayu Tallady <sarayutallady@example.com>
2025-12-04 20:38:58 +0530

    Fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
-import { miaodaDevPlugin } from "miaoda-sc-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
+  plugins: [
+    react(),
+    svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true,
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"),
     },
+  },
+
+  // Vercel needs this!
+  build: {
+    outDir: "dist",
   },
 });
